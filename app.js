4e1e17b824571c41fe75375f30d585f202c2169c
James Hunt <famouswelshman@yahoo.co.uk>
2022-09-27 20:38:11 +0000

    delete function and checkmarked added

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 //Selectors
-const todoInput = document.querySelector('.todo-input');
-const todoButton = document.querySelector('.todo-button');
-const todoList = document.querySelector('.todo-list');
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+const filterOption = document.querySelector(".filter-todo");
 
 //Event Listeners
 todoButton.addEventListener('click', addTodo);
+todoList.addEventListener('click', deleteCheck);
+filterOption.addEventListener('click', filterTodo);
 
 // Functions
 function addTodo(event) {
@@ -37,4 +40,40 @@ function addTodo(event) {
     todoList.appendChild(todoDiv);
     // Clear todo input value
     todoInput.value ="";
+}
+
+function deleteCheck(e) {
+    const item = e.target;
+    // Delete todo
+    if (item.classList[0] === "trash-btn") {
+        const todo = item.parentElement;
+        // Animation
+        todo.classList.add('fall');
+        todo.addEventListener('transitionend', function(){
+            todo.remove();
+        });
+    }
+
+    // checkmark
+    if(item.classList[0] === "complete-btn") {
+        const todo = item.parentElement;
+        todo.classList.toggle("completed");
+    }
+}
+
+function filterTodo(e){
+    const todos= todoList.childNodes;
+    todos.forEach(function(todo){
+        switch(e.target.value){
+            case "all":
+                todo.style.display = 'flex';
+                break;
+                case "completed":
+                    if(todo.classList.contains('completed')){
+                        todo.style.display = 'flex';
+                    }else{
+                        todo.style.display = "none";
+                    }
+        }
+    });
 }
